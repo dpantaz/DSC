1e31ba3e3906883f4de8a70349c32006cea7ba09
Dimitris Pantazis <pantazisd@outlook.com>
2023-01-25 12:44:24 +0200

    Initial Commit

---
+++ SetPageFile.ps1
@@ -0,0 +1,35 @@
+﻿Configuration SetPageFile {
+    
+    Import-DscResource -ModuleName ComputerManagementDsc
+    Import-DscResource -ModuleName StorageDSC
+
+    Node localhost {
+    
+        LocalConfigurationManager {
+            RebootNodeIfNeeded = $true
+        }
+
+        WaitforDisk Disk2
+        {
+            DiskId           = 2
+            RetryIntervalSec = 30
+            RetryCount       = 3
+        }
+
+        Disk Disk2 {
+            DiskId      = 2
+            DriveLetter = 'T'
+            DependsOn   = '[WaitForDisk]Disk2'
+        }
+
+        VirtualMemory PagingSettings
+        {
+            Type        = 'CustomSize'
+            Drive       = 'T'
+            InitialSize = '2048'
+            MaximumSize = '2048'
+            DependsOn   = '[Disk]Disk2'
+        }
+    }
+
+}
